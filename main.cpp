438b06c7cd25b749b293912b9d7b807cea6fac93
WB-FLPoly <WBlaine7546@floridapoly.edu>
2026-04-27 20:47:00 +0000

    Asdf.

--- main.cpp
+++ main.cpp
@@ -6,8 +6,25 @@
 #include <unordered_map>
 
 
-void printCommonFriends(const Graph<std::string>& friends) {
-
+void printCommonFriends(const Graph<std::string>& Friends) {
+    for(int i = 0; i < Friends.size()-1; ++i) {
+        for (int q = i+1; q < Friends.size(); ++q) {
+            std::string A = "";
+            for (int j = 0; j < Friends.edges[i].size(); ++j) {
+                for (int k = 0; k < Friends.edges[q].size(); ++k) {
+                    if (Friends.edges[i][j] == Friends.edges[q][k]) {
+                        A += Friends[Friends.edges[i][j]];
+                        A += " ";
+                    }
+                }
+            }
+            if (A != "") {
+                std::cout << Friends[i] << " and " << Friends[q] << " common friends : ";
+                std::cout << A;
+                std::cout << std::endl;
+            }
+        }
+    }
 }
 
 int main(void) {
@@ -21,7 +38,7 @@ int main(void) {
     
     input.close();
     
-    list.rotate_right(47);
+    //list.rotate_right(47);
     
     Graph<std::string> graph;
     while (!list.empty()) {
